f64e55320dfc08166905ede29fbcd84d258da140
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2018-06-20 16:18:12 +0300

    updated compression

--- ui/server.js
+++ ui/server.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const compression = require('compression');
 
-app.use((req) => {
-    console.log('headers', req.originalUrl, JSON.stringify(req.headers, null, 2));
-});
-app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
