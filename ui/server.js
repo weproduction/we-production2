8ec435ec11192832ee9857c2b015b5556f503ad3
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2018-06-20 16:59:34 +0300

    fixed server

--- ui/server.js
+++ ui/server.js
@@ -8,7 +8,12 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+let server = null;
 module.exports = {
-    start: () => app.listen(3000, () => console.log('Started')),
-    stop: () => app.stop()
+    start() {
+        server = app.listen(3000, () => console.log('Started'))
+    },
+    stop() {
+        server.close(() => console.log('Stopped'))
+    }
 };
