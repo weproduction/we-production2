1952789da53a7f07596b6a442b323e1363468d35
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2018-06-21 18:09:48 +0300

     fix

--- ui/server.js
+++ ui/server.js
@@ -60,7 +60,7 @@ nonSPA_Router.get('/videos/:category?/:tag?', (req,res, next) => {
 
         const host = req.headers.host;
         res.render('bot', {
-            url: `//${host}${req.originalUrl}`,
+            url: `http://${host}${req.originalUrl}`,
             type: 'video.other',
             image: video.preview[1080] || video.preview[720],
             title: `${video.title_en} ${locale_en.by} ${locale_en.title}`,
@@ -72,9 +72,9 @@ nonSPA_Router.get('/videos/:category?/:tag?', (req,res, next) => {
 nonSPA_Router.get('/*', (req,res) => {
     const host = req.headers.host;
     res.render('bot', {
-        url: `${host}${req.originalUrl}`,
+        url: `http://${host}${req.originalUrl}`,
         type: 'website',
-        image: `//${host}/img/fb-preview.jpg`,
+        image: `http//${host}/img/fb-preview.jpg`,
         title: locale_en.title,
         description: locale_en.description,
     });
