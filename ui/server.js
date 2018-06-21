a3382f948dcc8f1237aa9e4f259e7c674423f0ba
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2018-06-21 13:48:53 +0300

    fix

--- ui/server.js
+++ ui/server.js
@@ -13,7 +13,7 @@ const SPA_Router = express.Router();
 SPA_Router.use((req,res,next) => {
     const ua = req.headers['user-agent'];
 
-    if (/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(ua)) {
+    if (!/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(ua)) {
         next('router');
     } else {
         next();
@@ -31,23 +31,23 @@ nonSPA_Router.get('/video', (req,res) => {
     res.render('bot', {
         url: 'http://we-production.herokuapp.com/test-video',
         type: 'video.other',
-        image: 'https://i.vimeocdn.com/video/503244445_1920x1080.jpg?r=pad',
+        image: 'https://i.vimeocdn.com/video/503244445_1920x1080.jpg?r=padv',
         title: 'Waterdrops by We Production',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-    })
+    });
 });
 
-nonSPA_Router.get('*', (req,res) => {
+nonSPA_Router.get('/article', (req,res) => {
     res.render('bot', {
-        url: 'http://we-production.herokuapp.com    ',
+        url: 'http://we-production.herokuapp.com/',
         type: 'article',
-        image: 'http://we-production.herokuapp.com/img/logo@2x.png',
+        image: 'http://we-production.herokuapp.com/img/logo@4x.jpg',
         title: 'We Production',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-    })
+    });
 });
 
-app.use('*', SPA_Router, nonSPA_Router);
+app.use('/', SPA_Router, nonSPA_Router);
 
 let server = null;
 module.exports = {
