21e2c29918b15ebf5d0d64bd1c7add5b277ffebd
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2018-06-21 13:07:27 +0300

    added basic FB sharing integration

--- ui/server.js
+++ ui/server.js
@@ -2,16 +2,47 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.set('view engine', 'jade');
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.static(path.join(__dirname, 'build'), {
+    index: false
+}));
+
+const SPA_Router = express.Router();
+
+SPA_Router.use((req,res,next) => {
+    const ua = req.headers['user-agent'];
+
+    if (/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(ua)) {
+        next('router');
+    } else {
+        next();
+    }
 });
+
+SPA_Router.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'build', 'index.html')));
+
+const nonSPA_Router = express.Router({});
+
+/*nonSPA_Router.get('/en/videos/:category/:tag/:video', (req,res) =>
+    res.send('Serve regular HTML with metatags'));
+*/
+nonSPA_Router.get('*', (req,res) =>
+    res.render('bot', {
+        url: 'http://example.org',
+        type: 'website',
+        image: 'http://we-production.herokuapp.com/img/logo@2x.png',
+        title: 'We Production',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    }));
+
+app.use('*', SPA_Router, nonSPA_Router);
 
 let server = null;
 module.exports = {
     start() {
-        server = app.listen(3000, () => console.log('Started'))
+        server = app.listen(3000, () => console.log('Started 3000'))
     },
     stop() {
         server.close(() => console.log('Stopped'))
